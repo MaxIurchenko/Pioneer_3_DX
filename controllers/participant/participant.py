c979809cdc5e08806c0a4ea165978e8e62188da2
Maksim Iurchenko <123802094+MaxIurchenko@users.noreply.github.com>
2023-12-17 15:09:23 +0200

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -1,5 +1,3 @@
-"""rxr_demo_template controller."""
-
 from controller import Robot
 
 # Get pointer to the robot.
@@ -8,21 +6,23 @@ robot = Robot()
 # Get pointer to each wheel of our robot.
 leftWheel = robot.getDevice('left wheel')
 rightWheel = robot.getDevice('right wheel')
-leftWheel.setVelocity(5)
-rightWheel.setVelocity(5)
 
 # Repeat the following 4 times (once for each side).
 for i in range(0, 4):
     # First set both wheels to go forward, so the robot goes straight.
-    leftWheel.setPosition(200)
-    rightWheel.setPosition(200)
+    leftWheel.setVelocity(5)
+    rightWheel.setVelocity(5)
+    leftWheel.setPosition(1000)
+    rightWheel.setPosition(1000)
     # Wait for the robot to reach a corner.
     robot.step(4100)
     # Then, set the right wheel backward, so the robot will turn right.
+    leftWheel.setVelocity(3.14)
+    rightWheel.setVelocity(3.14)
     leftWheel.setPosition(100)
     rightWheel.setPosition(-100)
     # Wait until the robot has turned 90 degrees clockwise.
-    robot.step(500)
+    robot.step(800)
 
 # Stop the robot when path is completed, as the robot performance
 # is only computed when the robot has stopped.
